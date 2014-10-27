cc0905e8bcfa4bb32a4f3ffe4280768037d87ed7
megaplanet <megaplanet.github@megaplanet.org>
2014-10-27 20:55:10 +0100

    changed setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'alaocl',
+  packages = ['alaocl'],
+  version = '0.1',
+  description = 'ala OCL expressions for Python, OCL refering UML Object Constraint Language standard',
+  author = 'megaplanet',
+  author_email = 'megaplanet@megaplanet.org',
+  url = 'https://github.com/megaplanet/AlaOCL/',
+  download_url = 'https://github.com/megaplanet/AlaOCL/', 
+  keywords = ['OCL','constraint','UML','validation','query language'],
+  classifiers = [],
+)
