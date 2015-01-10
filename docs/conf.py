3a4c6c56ffc11a9849031e0a06aad586afcd0d6f
megaplanet <megaplanet.github@megaplanet.org>
2015-01-10 23:29:49 +0100

    added sphinx_rtd_theme trick for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -135,9 +135,15 @@ pygments_style = 'sphinx'
 
 # read the docs theme.
 # see https://github.com/snide/sphinx_rtd_theme
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 #--------------------------------------------------------------------------
 # <+JFE
 #--------------------------------------------------------------------------
