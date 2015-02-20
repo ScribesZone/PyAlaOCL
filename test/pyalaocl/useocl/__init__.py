974994817b72a6795a798823be2a0ac19675572d
megaplanet <megaplanet.github@megaplanet.org>
2015-02-20 17:16:38 +0100

    add method to get files from test directories

--- test/pyalaocl/useocl/__init__.py
+++ test/pyalaocl/useocl/__init__.py
@@ -4,6 +4,13 @@ import os
 TEST_CASES_DIRECTORY = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'testcases')
 
+TEST_SOIL_DIRECTORY = os.path.join(TEST_CASES_DIRECTORY, 'soil')
+
+def getUseFile(name):
+    return os.path.join(TEST_CASES_DIRECTORY, name)
+
+def getSoilFile(name):
+    return os.path.join(TEST_SOIL_DIRECTORY, name)
 
 def setup():
     pass
